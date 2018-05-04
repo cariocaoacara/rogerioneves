bf112e9461836d459187352c5e26f3d94985a562
51115152017.2 <51115152017.2@51DE194276.senacrjedu.local>
2018-05-03 21:52:25 -0300

    Segunda  aula  de  Jav

---
+++ src/exemplos/TiposDeVariaveis.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+public class TiposDeVariaveis {
+	
+	public static void main(String[] args) {
+		Integer inteiro = 10;
+		char letra = 'L';
+		String texto = "Primerira Aula de Java";
+		Double real = 28.10;
+		boolean situacao = true;
+	
+		
+		System.out.println(inteiro);
+		System.out.println(letra);
+		System.out.println(texto);
+		System.out.println(real);
+		System.out.println(situacao);
+		System.out.println(Integer.toBinaryString(inteiro));
+	}
+
+}
